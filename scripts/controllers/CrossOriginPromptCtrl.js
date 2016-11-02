4d5c37adf572c84baa262e4b163f5cd9e0efff76
Luqiang Tian <Luqiangtian@gmail.com>
2016-11-02 20:57:20 +0800

    [Codegen Prompt]: Disable Chrome download,because Chrome don't support saving file from js

--- scripts/controllers/CrossOriginPromptCtrl.js
+++ scripts/controllers/CrossOriginPromptCtrl.js
@@ -4,14 +4,16 @@
 SwaggerEditor.controller('CrossOriginPromptCtrl', function CrossOriginPromptCtrl($scope,
   $uibModalInstance, $rootScope, simpleYaml,YAML) {
   YAML.dump(simpleYaml.swagger, function(error, result){
-  var yamlBlob = new Blob([result], {type: 'text/plain'});
-  $scope.yamlDownloadHref = window.URL.createObjectURL(yamlBlob);
-  $scope.yamlDownloadUrl = [
-        'text/plain',
-        'swagger.yaml',
-        $scope.yamlDownloadHref
-      ].join(':');
-  
+  if(!window.chrome || !window.chrome.webstore)
+  {
+    var yamlBlob = new Blob([result], {type: 'text/plain'});
+    $scope.yamlDownloadHref = window.URL.createObjectURL(yamlBlob);
+    $scope.yamlDownloadUrl = [
+          'text/plain',
+          'swagger.yaml',
+          $scope.yamlDownloadHref
+        ].join(':');
+  }
   if(error) {$scope.swaggerSpec = "Error"; return;}
   {$scope.swaggerSpec = result; return;}
   });
